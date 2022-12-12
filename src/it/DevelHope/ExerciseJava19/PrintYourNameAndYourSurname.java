64318cb9757568ee6347d4088d6f0bc7eecf0e1d
Marco Lo Bello <xSalamander@outlook.it>
2022-12-12 20:35:00 +0100

    Initial Commit

---
+++ src/it/DevelHope/ExerciseJava19/PrintYourNameAndYourSurname.java
@@ -0,0 +1,9 @@
+package it.DevelHope.ExerciseJava19;
+
+public class PrintYourNameAndYourSurname {
+    public static void main(String[] args) {
+        String name = "Marco ", surname = "Lo Bello";
+        System.out.println(name);
+        System.out.println(surname);
+    }
+}
